3e4f3d2bbf2cd606fc99949851f8e8436ca82f3f
Pedro Ferreira <ferfonhp@gmail.com>
2025-10-09 20:55:47 -0300

    fix: logica correta bubble short

--- bubbleShort.py
+++ bubbleShort.py
@@ -7,7 +7,7 @@ def parallel_bubble_sort(arr):
 
     threads = []
     for i in range(0, len(arr), 2):
-        thread = threading.Thread(target=bubble_sort_pair, args=(arr, i, i+1))
+        thread = threading.Thread(target=bubble_sort_pair, args=(arr, i))
         threads.append(thread)
         thread.start()
 
@@ -16,21 +16,24 @@ def parallel_bubble_sort(arr):
 
     return arr
 
-def bubble_sort_pair(arr, i, j):
-    if arr[i] > arr[j-1]:
+# def bubble_sort_pair(arr, i, j):
+#     if arr[i] > arr[j-1]:
+#         arr[i], arr[j] = arr[j], arr[i]
+
+def bubble_sort_pair(arr, i): # Apenas o índice 'i' é necessário
+    j = i + 1
+    # Verifica se j está dentro dos limites (len(arr) - 1)
+    if j < len(arr) and arr[i] > arr[j]:
         arr[i], arr[j] = arr[j], arr[i]
 
-# def main():
-#     arr = np.load("A.npy")
-#     arr = arr[:10]
+def main():
+    arr = np.load("A.npy")
+    arr = arr[:10]
     
-#     print("Lista original:", arr)
-    
-#     sorted_arr = parallel_bubble_sort(arr)
+    print("Lista original:", arr)
+    sorted_arr = parallel_bubble_sort(arr)
+    print("Lista ordenada:", sorted_arr)
+    return 0
 
-#     print("Lista ordenada:", sorted_arr)
-    
-#     return 0
-
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
